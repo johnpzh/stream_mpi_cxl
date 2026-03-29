94a4cca555fac2b19aef8dfa758e7f41baedead2
Zhen Peng <hi.pengzhen@gmail.com>
2026-03-28 23:17:22 -0700

    Use -x in mpirun to make effects for all nodes

--- playground1_init/scripts/run00.stream_mpi.with_cxl.2-nodes.sh
+++ playground1_init/scripts/run00.stream_mpi.with_cxl.2-nodes.sh
@@ -9,21 +9,24 @@ mkdir -p "$OUTPUT_DIR"
 cd "$OUTPUT_DIR"
 
 :> output.with_cxl.log
-:> output.with_cxl.realtime.log
+#:> output.with_cxl.realtime.log
 
 binary="../../build/stream_mpi_cxl"
 
 set -x
 {
-export CXL_DAX_PATH="/dev/dax1.0" #32GB CXL device on target_node3
-export CXL_MEASURE_LATENCY=1
-LD_PRELOAD="${cxl_lib_file}" \
-/usr/bin/time -f "%e" \
+#export CXL_DAX_PATH="/dev/dax1.0" #32GB CXL device on target_node3
+#export CXL_MEASURE_LATENCY=1
+#LD_PRELOAD="${cxl_lib_file}" \
+#/usr/bin/time -f "%e" \
 mpirun -x BADSSERVER=twosisters2:50505 \
   --hostfile "${hostfile}" \
   --map-by ppr:1:node \
   -np 2 \
+  -x CXL_DAX_PATH="/dev/dax1.0" \
+  -x CXL_MEASURE_LATENCY=1 \
+  -x LD_PRELOAD="${cxl_lib_file}" \
   "${binary}"
 } 2>&1 | tee -a output.with_cxl.log
 set +x
-tail -n 1 output.with_cxl.log >> output.with_cxl.realtime.log
+#tail -n 1 output.with_cxl.log >> output.with_cxl.realtime.log
--- playground1_init/scripts/run01.stream_mpi.no_cxl.2-nodes.sh
+++ playground1_init/scripts/run01.stream_mpi.no_cxl.2-nodes.sh
@@ -8,13 +8,13 @@ mkdir -p "$OUTPUT_DIR"
 cd "$OUTPUT_DIR"
 
 :> output.no_cxl.log
-:> output.no_cxl.realtime.log
+#:> output.no_cxl.realtime.log
 
 binary="../../build/stream_mpi_cxl"
 
 set -x
 {
-/usr/bin/time -f "%e" \
+#/usr/bin/time -f "%e" \
 mpirun -x BADSSERVER=twosisters2:50505 \
   --hostfile "${hostfile}" \
   --map-by ppr:1:node \
@@ -22,4 +22,4 @@ mpirun -x BADSSERVER=twosisters2:50505 \
   "${binary}"
 } 2>&1 | tee -a output.no_cxl.log
 set +x
-tail -n 1 output.no_cxl.log >> output.no_cxl.realtime.log
+#tail -n 1 output.no_cxl.log >> output.no_cxl.realtime.log
